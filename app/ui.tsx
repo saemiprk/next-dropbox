af1477983f905984df6523fb5cb0db6b417d4759
Saemi Park <saemiprk@gmail.com>
2024-08-13 14:26:25 +0900

    first commit

---
+++ app/ui.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function UI(){
+
+    return (
+        <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
+            dd
+        </div>
+    )
+}
---
+++ types_db.ts
@@ -0,0 +1,3 @@
+Need to install the following packages:
+  supabase@1.188.4
+Ok to proceed? (y) 
