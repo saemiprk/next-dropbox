a53e1db07f53ff8a6498022d795db5ffaac48cf6
Saemi Park <saemiprk@gmail.com>
2024-08-16 17:33:28 +0900

    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import UI from "components/ui";
 
+export const metadata = {
+  title: "Minibox",
+  description: "Minibox for file upload"
+}
+
 export default function Home() {
   return (
       <UI />
